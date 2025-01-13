d6bbb31585d98fcfa44ee32c2c77d4089e998478
Ben Mens <ben.mens@gmail.com>
2025-01-13 19:52:36 +0100

    - reduced requirements.txt to root libraries - added pgzero - added pygame - added more tests

--- test p5.py
+++ test p5.py
@@ -1,5 +1,9 @@
 from p5 import *
 
+mouse_x: sketch.events.builtins.mouse_x
+mouse_y: sketch.events.builtins.mouse_y
+mouse_is_pressed: sketch.events.builtins.mouse_is_pressed
+
 
 def setup():
     size(640, 360)
@@ -8,16 +12,14 @@ def setup():
 
 
 def draw():
-    if sketch.events.builtins.mouse_is_pressed:
+    if mouse_is_pressed:
         fill(random_uniform(255), random_uniform(127), random_uniform(51), 127)
     else:
         fill(255, 15)
 
     circle_size = random_uniform(low=10, high=80)
 
-    circle(
-        (sketch.events.builtins.mouse_x, sketch.events.builtins.mouse_y), circle_size
-    )
+    circle((mouse_x, mouse_y), circle_size)
 
 
 def key_pressed(_event):
